72c41f8b29f835bab1845c34d3fc445bdaee8aa8
Pomelodies <randomelodies@gmail.com>
2025-11-11 14:47:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import data from "./assets/data.json";
+import logo from "./img/logo.png";
+import "./App.css";
+// console.log(data);
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="Logo Netflix" />
+      <main>
+        <section>
+          <p>Emission TV</p>
+          <div>
+            <article>Img 1</article>
+            <article>Img 2</article>
+            <article>Img 3</article>
+            <article>Img 4</article>
+            <article>Img 5</article>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
