333a42c2c8f4e30d60555e9155708418bc2a67d2
Pomelodies <randomelodies@gmail.com>
2025-11-11 15:16:24 +0100

    Section added

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,111 @@ function App() {
       <header>
         <img src={logo} alt="Logo Netflix" />
       </header>
-
       <main>
         <section>
-          <p>Emission TV</p>
+          <h2>Emissions TV</h2>
           <div>
-            <article>Img 1</article>
-            <article>Img 2</article>
-            <article>Img 3</article>
-            <article>Img 4</article>
-            <article>Img 5</article>
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642943/react-new-exercices/netflix2022/reprendre%20avec/AAAABd4eOiOZFaZT1-9l68Ra0uC-4kl3zMSPkeA5TyDu8v_KNgAAvifmM8boe5G2kexKSdk2uznXcDIJIlBoz4RA6p-q0_E_xg9maq.webp"
+              alt="Emmission1"
+            />
+          </div>
+        </section>
+        <section>
+          <h2>Séries françaises</h2>
+          <div>
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
+            <img
+              src="https://res.cloudinary.com/lereacteur-apollo/image/upload/v1643642872/react-new-exercices/netflix2022/com%C3%A9dies/AAAABequpfcLQKRfF0HJAEuqNqj2ajBpEIsoQqhy5yceU7sNrF7qNRTQImbamREIEwo0ChfReGxSP8v4kzIJb_-9ml_u0RRsZcWOR4Tp3Mz_sxxXGiubxViheD3FYDWU_gjxaud.jpg"
+              alt="Emmission1"
+            />
           </div>
         </section>
       </main>
